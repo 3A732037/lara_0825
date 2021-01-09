4e0f698455f3f37666c730ed449ed3234c1ce15f
3a732037 <3a732037@gm.student.ncut.edu.tw>
2021-01-10 02:43:15 +0800

    (0825)練習3.2 使用 all方法

--- routes/web.php
+++ routes/web.php
@@ -1,35 +1,21 @@
 <?php
-use App\Http\Controllers\PostsController;
-use App\Models\Post;
-use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+namespace App\Models;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-Route::get('/posts',[PostsController::class,'index'])->name('posts.index');
-Route::get('/post',[PostsController::class,'show'])->name('posts.show');
-Route::get('/about',[PostsController::class,'about'])->name('posts.about');
-Route::get('/contact',[PostsController::class,'contact'])->name('posts.contact');
+class Post extends Model
+{
+    use HasFactory;
 
+    protected $table='posts';
 
-$post=new Post();
-$post->title ='test title';
-$post->content ='test content';
-$post->save();
+    protected $fillable = [
+        'title',
+        'content',
+        'is_feature',
+        'content-test'
+    ];
 
-Post::create([
-    'title'=>'test title',
-    'content'=>'test content',
-]);
+}
